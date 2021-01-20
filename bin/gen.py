9b533f3b6f87fa9daa26ecafc6544892636673f9
Andre Torrez <andre@torrez.org>
2021-01-19 20:49:26 -0800

    Q&D make page.

---
+++ bin/gen.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+all_lines = []
+big_string = ""
+
+template = """
+<iframe width="560" height="315" src="https://www.youtube.com/embed/XXXXXXXXXX" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+"""
+with open('source.txt','r') as fh:
+     all_lines = fh.readlines()
+
+for line in all_lines:
+    trash, yt_code = line.rstrip().split('=')
+
+    embed = template.replace('XXXXXXXXXX', yt_code) 
+    big_string += embed + "<br>"
+
+print(big_string)
